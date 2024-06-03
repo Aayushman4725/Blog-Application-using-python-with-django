dcbdc3c7501833cd1bd02480bc718c1470983aaf
Aayushman4725 <aayushmannp@gmail.com>
2024-06-03 22:10:29 +0545

    updated urls.py and views.py

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,9 @@ from . import views
 
 
 urlpatterns = [
+     
+     path('blog/',views.blog,name='blog'),
     path('add_blog/', views.submitBlog, name='add_blog'),
+    path('sign_in/', views.sign_in, name='sign_in'),
+    path('sign_up/',views.signup, name='sign_up'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,39 @@
 from django.shortcuts import render,redirect
 from blog.models import Blog
+from django.contrib.auth.models import User
 
 # Create your views here.
+
+
+
 def submitBlog(request):
     blog_title=request.POST['postTitle']
     blog=request.POST['postContent']
     Blog.objects.create(title=blog_title, blog=blog)
     return redirect('home')
+
+def signup(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        confirm = request.POST['confirm-password']
+
+        myUser = User.objects.create_user(username,email,password)
+
+        myUser.save()
+        return redirect('sign_in')
+    
+    return render(request, 'signup.html')
+
+
+def blog(request):
+    blog = Blog.objects.all()
+
+    context ={
+        'blog' : blog
+    }
+    return render(request, 'index.html', context)
+
+def sign_in(request):
+    return redirect('blog')
