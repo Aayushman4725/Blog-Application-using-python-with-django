16da4871a4278a7eac91b864825a55740b2e39e3
Aayushman4725 <aayushmannp@gmail.com>
2024-06-04 13:42:29 +0545

    updated files

--- blog/urls.py
+++ blog/urls.py
@@ -7,6 +7,5 @@ urlpatterns = [
      
      path('blog/',views.blog,name='blog'),
     path('add_blog/', views.submitBlog, name='add_blog'),
-    path('sign_in/', views.sign_in, name='sign_in'),
-    path('sign_up/',views.signup, name='sign_up'),
+   
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render,redirect
 from blog.models import Blog
-from django.contrib.auth.models import User
+
 
 # Create your views here.
 
@@ -10,21 +10,9 @@ def submitBlog(request):
     blog_title=request.POST['postTitle']
     blog=request.POST['postContent']
     Blog.objects.create(title=blog_title, blog=blog)
-    return redirect('home')
+    return redirect('blog')
 
-def signup(request):
-    if request.method == 'POST':
-        username = request.POST['username']
-        email = request.POST['email']
-        password = request.POST['password']
-        confirm = request.POST['confirm-password']
 
-        myUser = User.objects.create_user(username,email,password)
-
-        myUser.save()
-        return redirect('sign_in')
-    
-    return render(request, 'signup.html')
 
 
 def blog(request):
@@ -35,5 +23,3 @@ def blog(request):
     }
     return render(request, 'index.html', context)
 
-def sign_in(request):
-    return redirect('blog')
