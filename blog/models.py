c8f6d6944fc620dd76c9bf326c9271d63331a3ba
Aayushman4725 <aayushmannp@gmail.com>
2024-06-07 22:00:00 +0545

    updated the blog model

--- blog/models.py
+++ blog/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Blog(models.Model):
     title = models.CharField(max_length=100)
-    blog = models.CharField(max_length=500)
+    blog = models.TextField(max_length=500)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
