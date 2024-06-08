1c195abc9429151923abfab84bdc255ab45b2d8e
Aayushman4725 <aayushmannp@gmail.com>
2024-06-08 21:58:14 +0545

    added some paths for CRUD operations

--- blog/urls.py
+++ blog/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
 
 from . import views
-
+from  .views import BlogList,createBlog,EditBlog,DeleteBlog
 
 urlpatterns = [
      
-     path('home_blog/',views.blog,name='blog'),
-    path('add_blog/', views.submitBlog, name='add_blog'),
+    
+    # path('add_blog/', views.submitBlog, name='add_blog'),
+    path('create_blog/', createBlog.as_view(), name='create_blog'),
+    path('edit_blog/<int:pk>',EditBlog.as_view() , name='edit_blog'),
+    path('delete_blog/<int:pk>',DeleteBlog.as_view() , name='delete_blog'),
    
 ]
