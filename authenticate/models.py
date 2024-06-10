9853815aecf2df2be818527f9b7fa4bdd13a9ce7
Aayushman4725 <aayushmannp@gmail.com>
2024-06-10 21:33:43 +0545

    added custom user model

--- authenticate/models.py
+++ authenticate/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
+from .managers import CustomUserManager
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    username = models.CharField(_("Username"), max_length=100, unique=True)
+    profile_picture = models.ImageField(_("Profile"), blank=True, null=True)
+    email = models.EmailField(_("Email"), unique=True)
+    phone_number = models.CharField(_("Phone Number"),max_length=15, blank=True, null=True)
+    about = models.TextField(_("About"), blank=True, null=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']  # Add 'username' to REQUIRED_FIELDS
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.username
