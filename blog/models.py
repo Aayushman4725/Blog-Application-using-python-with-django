43df989a537eed0f85e351eb572c4a7e1a590c7a
Aayushman4725 <aayushmannp@gmail.com>
2024-06-10 21:37:22 +0545

    implemented custom user model

--- blog/models.py
+++ blog/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.conf import settings
 
 # Create your models here.
 class Blog(models.Model):
-    user = models.ForeignKey(User,on_delete=models.CASCADE,null=True, blank=True)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE,null=True, blank=True)
     title = models.CharField(max_length=100)
     blog = models.TextField(max_length=500)
     created_at = models.DateTimeField(auto_now_add=True)
