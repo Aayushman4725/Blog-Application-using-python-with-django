6bb55f8b1b49978e68a8e643ce413493b6849cb3
Aayushman4725 <aayushmannp@gmail.com>
2024-06-23 21:36:33 +0545

    added model for likes

--- blog/models.py
+++ blog/models.py
@@ -9,6 +9,20 @@ class Blog(models.Model):
     blog = models.TextField(max_length=500)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    likes = models.IntegerField(default=0)
 
     def __str__(self):
         return self.title
+    
+    @property
+    def likes_count(self):
+        return self.like_set.count()
+    
+class Like(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    blog = models.ForeignKey('Blog', on_delete=models.CASCADE)
+    liked_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('user', 'blog')
+
