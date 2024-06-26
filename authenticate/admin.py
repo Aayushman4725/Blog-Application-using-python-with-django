13cb2242bb3a7ee596b9268f494a4d14c9a375a3
Aayushman4725 <aayushmannp@gmail.com>
2024-06-26 20:44:58 +0545

    added new register for profile model

--- authenticate/admin.py
+++ authenticate/admin.py
@@ -2,6 +2,7 @@
 
 # Register your models here.
 from django.contrib import admin
-from .models import CustomUser
+from .models import CustomUser,Profile
 # Register your models here.
 admin.site.register(CustomUser)
+admin.site.register(Profile)
